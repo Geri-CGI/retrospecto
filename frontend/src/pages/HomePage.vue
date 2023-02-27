561671465a821f961771736e70bba8debb4bc889
gergely.dallos <gergely.dallos@cgi.com>
2023-02-27 17:18:57 +0100

    adjusted flex grid for smaller screens
    - added calculatd screen size text size

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="q-mx-auto">
-    <div class="row">
-      <div class="col text-center">
-        <q-parallax :height="400" src="~assets/stock.jpg">
-          <div class="col text-center">
-            <h1 class="text-white text-bold">retrospecto</h1>
-            <h3 class="text-white">The best open source retrospective tool</h3>
-          </div>
-        </q-parallax>
-      </div>
+    <div>
+      <q-parallax :height="400" src="~assets/stock.jpg">
+        <div :class="getScreenSizeForTitle()" class="text-white text-bold text-center">retrospecto</div>
+        <div :class="getScreenSizeForSubTitle()" class="text-white text-center">The best open source
+          retrospective tool
+        </div>
+      </q-parallax>
     </div>
     <div class="row">
-      <div class="col full-height">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
         <q-card class="my-card full-height" flat square>
           <div class="text-center">
             <q-icon name="dashboard" color="blue" size="6.4em" style="padding: 30px"/>
@@ -24,13 +22,9 @@
               to help facilitate it.
             </div>
           </q-card-section>
-
-          <q-card-section class="q-pt-none">
-            {{ lorem }}
-          </q-card-section>
         </q-card>
       </div>
-      <div class="col">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
         <q-card class="my-card full-height" flat square>
           <div class="text-center">
             <q-icon name="style" color="blue" size="6.4em" style="padding: 30px"/>
@@ -42,13 +36,9 @@
               numbered cards face-down to the table, instead of speaking them aloud.
             </div>
           </q-card-section>
-
-          <q-card-section class="q-pt-none">
-            {{ lorem }}
-          </q-card-section>
         </q-card>
       </div>
-      <div class="col">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
         <q-card class="my-card full-height" flat square>
           <div class="text-center">
             <q-icon name="chat" color="blue" size="6.4em" style="padding: 30px"/>
@@ -60,33 +50,23 @@
               Come and have a talk with anyone. We do not store any history of the chat.
             </div>
           </q-card-section>
-
-          <q-card-section class="q-pt-none">
-            {{ lorem }}
-          </q-card-section>
         </q-card>
       </div>
     </div>
-    <div class="row">
-      <div class="col">
-        <q-parallax :height="400" src="~assets/stock.jpg">
-          <div class="col text-center vertical-middle">
-            <h1 class="text-white text-bold">contribute</h1>
-            <a href="https://github.com/Geri-CGI/retrospecto" target="_blank"><h3 class="text-white">
-              https://github.com/Geri-CGI/retrospecto</h3></a>
-          </div>
-        </q-parallax>
-      </div>
+    <div>
+      <q-parallax :height="400" src="~assets/stock.jpg">
+        <div :class="getScreenSizeForTitle()" class="text-white text-bold text-center">contribute</div>
+        <div :class="getScreenSizeForSubTitle()" class="text-white text-center"><a
+          href="https://github.com/Geri-CGI/retrospecto" target="_blank">
+          https://github.com/Geri-CGI/retrospecto</a></div>
+      </q-parallax>
     </div>
     <div class="row">
-      <div class="col full-height">
-      </div>
-      <div class="col full-height">
+      <div class="col">
         <q-card class="my-card full-height" flat square>
           <div class="text-center">
             <q-icon name="help" color="blue" size="6.4em" style="padding: 30px"/>
           </div>
-
           <q-card-section>
             <div class="text-h3 text-center" style="padding-bottom: 20px">Good to know</div>
             <div class="text-subtitle2 text-center">Retrospecto is not storing any data on a long term. The board, poker
@@ -94,11 +74,7 @@
               using any database, so your data is safe.
             </div>
           </q-card-section>
-
-
         </q-card>
-      </div>
-      <div class="col full-height">
       </div>
     </div>
   </div>
@@ -106,8 +82,49 @@
 
 <script>
 import {defineComponent} from 'vue'
+import {Screen} from 'quasar'
 
 export default defineComponent({
-  name: 'HomePage'
+  name: 'HomePage',
+  created() {
+    window.addEventListener("resize", this.getScreenSizeForTitle)
+    window.addEventListener("resize", this.getScreenSizeForSubTitle)
+  },
+  methods: {
+    getScreenSizeForTitle() {
+      if (Screen.xs) {
+        return "text-h4"
+      }
+      if (Screen.sm) {
+        return "text-h3"
+      }
+      if (Screen.md) {
+        return "text-h1"
+      }
+      if (Screen.lg) {
+        return "text-h1"
+      }
+      if (Screen.xl) {
+        return "text-h1"
+      }
+    },
+    getScreenSizeForSubTitle() {
+      if (Screen.xs) {
+        return "text-h6"
+      }
+      if (Screen.sm) {
+        return "text-h5"
+      }
+      if (Screen.md) {
+        return "text-h3"
+      }
+      if (Screen.lg) {
+        return "text-h3"
+      }
+      if (Screen.xl) {
+        return "text-h3"
+      }
+    }
+  }
 })
 </script>
